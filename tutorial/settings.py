a21fd26b954a3780b454c3df416b979c39ad0a9b
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-07 09:51:51 +0700

    End of quickstart project - Created serializers, viewsets and url router.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django Rest Framework (DRF)
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
